40e639f8b411726043593da008e14cb668c2c05d
Seokwon James Yang <seyan@microsoft.com>
2019-04-04 19:45:40 -0700

    set port value

--- app.py
+++ app.py
@@ -86,4 +86,4 @@ def index():
             return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=8080, debug=True)
